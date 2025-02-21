ab65530a95ace8f7214dedc171ce7eb282665bcc
Jarosław Szewczyk <jaroslaw.szewczyk@tutamail.com>
2025-02-21 18:31:27 +0000

    Init client

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+
+const App = () => {
+  return (
+  <>
+    <CssBaseline />
+    <Container maxWidth="lg">
+      
+    </Container>
+  </>
+  )
+}
+
+export default App;
